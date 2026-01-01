05dba207a82c8b475fcf1ec769e34becef7ea37b
Ajihullah md <bipul.oji@gmail.com>
2026-01-01 22:03:20 +0100

    add mobile menu and animations

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const menuToggle = document.getElementById("menuToggle");
 const navMenu = document.getElementById("navMenu");
+const body = document.body;
 
-menuToggle.addEventListener("click", () => {
+menuToggle.addEventListener("click", (e) => {
+  e.stopPropagation();
+
+  menuToggle.classList.toggle("active");
   navMenu.classList.toggle("active");
+  body.classList.toggle("menu-open");
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    navMenu.classList.contains("active") &&
+    !navMenu.contains(e.target) &&
+    !menuToggle.contains(e.target)
+  ) {
+    navMenu.classList.remove("active");
+    menuToggle.classList.remove("active");
+    body.classList.remove("menu-open");
+  }
 });
